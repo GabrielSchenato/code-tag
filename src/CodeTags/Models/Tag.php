5499d9bad970984ae1eee267d91c88c7ac3202ca
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-03 16:28:19 -0300

    refatorado class tag

---
+++ src/CodeTags/Models/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodePress\CodeTags\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of Category
+ *
+ * @author gabriel
+ */
+class Tag extends Model
+{
+
+    protected $table = "codepress_tags";
+    protected $fillable = [
+        'name'
+    ];
+    
+    public function taggable()
+    {
+        return $this->morphTo();
+    }
+
+}
