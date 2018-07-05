b8dd1dd0333b2d1089a0405bb718ea0d48b53075
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-05 09:56:29 -0300

    tests model

--- src/CodeTag/Models/Tag.php
+++ src/CodeTag/Models/Tag.php
@@ -3,6 +3,7 @@
 namespace CodePress\CodeTag\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Validation\Validator;
 
 /**
  * Description of Category
@@ -16,7 +17,32 @@ class Tag extends Model
     protected $fillable = [
         'name'
     ];
-    
+    private $validator;
+
+    function getValidator()
+    {
+        return $this->validator;
+    }
+
+    public function setValidator(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function isValid()
+    {
+        $validator = $this->validator;
+        $validator->setRules(['name' => 'required|max:255']);
+        $validator->setData($this->attributes);
+
+        if ($validator->fails()) {
+            $this->errors = $validator->errors();
+            return false;
+        }
+
+        return true;
+    }
+
     public function taggable()
     {
         return $this->morphTo();
--- tests/CodeTag/Models/TagTest.php
+++ tests/CodeTag/Models/TagTest.php
@@ -4,9 +4,11 @@ namespace CodePress\CodeTag\Tests\Models;
 
 use CodePress\CodeTag\Models\Tag;
 use CodePress\CodeTag\Tests\AbstractTestCase;
+use Illuminate\Validation\Validator;
+use Mockery as m;
 
 /**
- * Description of CategoryTest
+ * Description of TagTest
  *
  * @author gabriel
  */
@@ -22,6 +24,60 @@ class TagTest extends AbstractTestCase
     public function test_check_if_a_tag_has_a_name()
     {
         $tag = Tag::create(['name' => 'Tag Test']);
+        $this->assertEquals('Tag Test', $tag->name);
+    }
+
+    public function test_inject_validator_in_tag_model()
+    {
+        $tag = new Tag();
+        $validator = m::mock(Validator::class);
+        $tag->setValidator($validator);
+
+        $this->assertEquals($tag->getValidator(), $validator);
+    }
+
+    public function test_should_check_if_it_is_valid_when_it_is()
+    {
+        $tag = new Tag();
+
+        $tag->name = 'Tag Test';
+
+        $validator = m::mock(Validator::class);
+        $validator->shouldReceive('setRules')->with(['name' => 'required|max:255']);
+        $validator->shouldReceive('setData')->with(['name' => 'Tag Test']);
+        $validator->shouldReceive('fails')->andReturn(false);
+
+        $tag->setValidator($validator);
+
+        $this->assertTrue($tag->isValid());
+    }
+
+    public function test_should_check_if_it_is_invalid_when_it_is()
+    {
+        $tag = new Tag();
+
+        $tag->name = 'Tag Test';
+
+        $messageBag = m::mock(Illuminate\Support\MessageBag::class);
+
+        $validator = m::mock(Validator::class);
+        $validator->shouldReceive('setRules')->with(['name' => 'required|max:255']);
+        $validator->shouldReceive('setData')->with(['name' => 'Tag Test']);
+        $validator->shouldReceive('fails')->andReturn(true);
+        $validator->shouldReceive('errors')->andReturn($messageBag);
+
+        $tag->setValidator($validator);
+
+        $this->assertFalse($tag->isValid());
+        $this->assertEquals($messageBag, $tag->errors);
+    }
+
+    public function test_check_if_a_tag_can_be_persisted()
+    {
+        $tag = Tag::create(['name' => 'Tag Test', 'active' => true]);
+        $this->assertEquals('Tag Test', $tag->name);
+
+        $tag = Tag::all()->first();
         $this->assertEquals('Tag Test', $tag->name);
     }
 
