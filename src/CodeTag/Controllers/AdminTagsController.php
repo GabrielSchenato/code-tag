72b6ca94bfd95d9ba446113c8ba2e1243f0ecf8e
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-19 18:00:34 -0300

    Autorizando areas

--- src/CodeTag/Controllers/AdminTagsController.php
+++ src/CodeTag/Controllers/AdminTagsController.php
@@ -19,29 +19,34 @@ class AdminTagsController extends Controller
     
     public function __construct(ResponseFactory $response, TagRepositoryInterface $repository)
     {
+        
         $this->repository = $repository;
         $this->response = $response;
     }
     
     public function index()
     {
+        $this->auth();
         $tags = $this->repository->all();
         return $this->response->view('codetag::index', compact('tags'));
     }
     
     public function show(int $id)
     {
+        $this->auth();
         $tag = $this->repository->find($id);
         return $this->response->view('codetag::show', compact('tag'));
     }
     
     public function create()
     {
+        $this->auth();
         return $this->response->view('codetag::create');
     }
     
     public function store(Request $request)
     {
+        $this->auth();
         $this->repository->create($request->all());
         
         return redirect()->route('admin.tags.index');
@@ -49,12 +54,14 @@ class AdminTagsController extends Controller
     
     public function edit(int $id)
     {
+        $this->auth();
         $tag = $this->repository->find($id);
         return $this->response->view('codetag::edit', compact('tag'));
     }
     
     public function update(int $id, Request $request)
     {
+        $this->auth();
         $this->repository->update($request->all(), $id);
         
         return redirect()->route('admin.tags.index');
@@ -62,19 +69,27 @@ class AdminTagsController extends Controller
     
     public function destroy(int $id)
     {
+        $this->auth();
         $this->repository->find($id)->delete();
         return redirect()->route('admin.tags.index');
     }
     
     public function deleted()
     {
+        $this->auth();
         $tags = $this->repository->deleted();
         return $this->response->view('codetag::deleted', compact('tags'));
     }
     
     public function restore(int $id)
     {
+        $this->auth();
         $this->repository->restore($id);
         return redirect()->route('admin.tags.index');
+    }
+    
+    private function auth()
+    {
+        $this->authorize('access_tags');
     }
 }
