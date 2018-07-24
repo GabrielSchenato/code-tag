35c975092009caad0be239384e5926d31ccd5514
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-24 18:16:58 -0300

    correção e ajustes finais

---
+++ tests/stub/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CodePress\CodeTag\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use CodePress\CodeTag\Models\Tag;
+
+/**
+ * Description of Post
+ *
+ * @author gabriel
+ */
+class Post extends Model
+{
+    const STATE_PUBLISHED = 1;
+    const STATE_DRAFT = 2;
+
+    use Sluggable, SoftDeletes;
+
+    protected $table = "codepress_posts";
+    protected $dates = ['deleted_at'];
+    protected $fillable = [
+        'title', 'image', 'content', 'slug'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable', 'codepress_taggables');
+    }
+
+}
